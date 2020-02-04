e65dd6f7590cb70fa7661fe98475557267a0e4ab
Juanma <juanmdiazrivas95@gmail.com>
2020-02-04 17:19:27 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
--- src/app/effects/currencyEffects.ts
+++ src/app/effects/currencyEffects.ts
@@ -11,6 +11,15 @@ import { switchMap, map } from 'rxjs/operators'
 
 @Injectable()
 export class CurrencyEffects {
+  @Effect()
+  update$: Observable<Action> = this.actions$.pipe(
+    ofType(currency.CURRENCIESUPDATE),
+    switchMap(() =>
+      this.currencyService
+        .getRates()
+        .pipe(map(data => new CurrenciesUpdatedAction(data)))
+    )
+  )
 
   constructor(
     private currencyService: CurrencyService,
